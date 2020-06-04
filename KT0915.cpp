af9234dfc2ffd99df17a5e3ddca1184c6377e6cf
Ricardo Lima Caratti <ricardo.caratti@adm.cogect>
2020-06-04 01:41:48 -0300

    KT0915 Arduino Library

--- KT0915.cpp
+++ KT0915.cpp
@@ -1,12 +1,11 @@
 /**
  * @file KT0915.cpp
  * @author Ricardo LIma Caratti (you@domain.com)
- * @brief PU2CLR KT0915 Arduino Library
+ * @brief  PU2CLR KT0915 Arduino Library
  * @details KT0915 Arduino Library implementation. This is an Arduino library for the KT0915, BROADCAST RECEIVER.  
  * @details It works with I2C protocol and can provide an easier interface for controlling the KT0915 device.<br>
  * @details This library was built based on KT0915 Datasheet from KTMicro (Monolithic Digital FM/MW/SW/LW Receiver Radio-on-a-Chip TM). 
- * 
- * This library can be freely distributed using the MIT Free Software model.
+ * @details This library can be freely distributed using the MIT Free Software model.
  * @copyright Copyright (c) 2020 Ricardo Lima Caratti. 
  */
 
